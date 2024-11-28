fac3aafe8b5b4aeffbb2afae15a9ddf18d3865d7
gael- <iffgabriel410@gmail.com>
2024-11-28 11:39:46 -0300

    Add files via upload
    cctccttctcttctccttc

---
+++ receita federal.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+#variáveis
+
+bd = mysql.connector.connect(host="localhost", user="root")
+
+cursor = mydb.cursor(dictionary = true))
+
+cursor.execute('''CREATE DATABASE IF NOT EXISTS vitima (
